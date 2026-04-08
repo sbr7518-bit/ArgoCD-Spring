01d11af7d575b47c874cdd3ddc7ad3addf7ae03d
Bora <sbr7518@gmail.com>
2026-04-08 15:33:06 +0900

    test

---
+++ src/main/java/kr/co/gitops/HomeController.java
@@ -0,0 +1,15 @@
+package kr.co.gitops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// ㅁㄴㅇㅁㄴㅇ
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "버전 1.0 - cicd 테스트 입니다.";
+	}
+
+}
